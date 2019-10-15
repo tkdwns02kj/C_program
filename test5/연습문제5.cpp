91424d949b26ff7ec66d1b072a060e08bf76c362
상준 박 <sumin02kj@naver.com>
2019-10-15 18:14:05 +0900

    124

---
+++ test5/연습문제5.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+	double a = 0;
+	double b = 0;
+	double c = 0;
+
+	scanf_s("%lf, %lf, %lf", &a, &b, &c);
+	printf("%0.4lf\n%0.4lf\n%0.4lf\n", a, b, c);
+
+	double d = 0;
+	d = (9.0 / 5.0) * a + 32.0;
+	printf("%0.4lf\n", d);
+
+	double e = 0;
+	e = (9.0 / 5.0) * b + 32.0;
+	printf("%0.4lf\n", e);
+
+	double f = 0;
+	f = (9.0 / 5.0) * c + 32.0;
+	printf("%0.4lf\n", f);
+	return 0;
+}
